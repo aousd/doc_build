d47089c6b0d51abcdac3224ae972dfcdb8c2c3e1
Paul Molodowitch <pmolodowitch@nvidia.com>
2026-03-06 12:00:02 -0800

    get_repo_root: don't make assumptions about repository structure

--- tests/build_scripts/build_docs.py
+++ tests/build_scripts/build_docs.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python3
 
 from doc_build.doc_builder import DocBuilder
+from pathlib import Path
 
+test_root = Path(__file__).parent.parent
 
 class MyDocBuilder(DocBuilder):
     pass
 
 
 if __name__ == "__main__":
-    MyDocBuilder().process_argparser()
+    MyDocBuilder(repo_root=test_root).process_argparser()
